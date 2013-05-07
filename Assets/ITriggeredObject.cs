545213a89840878a3e72d9f8c0abb748beada1df
David Hoogenbosch <DavidHoogenbosch@hotmail.com>
2013-05-07 13:47:04 +0200

    Updated meta files
    Added a comment to ItriggerObject interface

--- Assets/ITriggeredObject.cs
+++ Assets/ITriggeredObject.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public interface ITriggeredObject {
-
+	//dennis will implement a reaction class
 	void OnTriggered(EventReaction eventReaction, TriggerType triggerType);
 }
