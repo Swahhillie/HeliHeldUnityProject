6fe668597e05506ef3c8076d2db2066842a3d737
David Hoogenbosch <davidhoogenbosch@hotmail.com>
2013-05-12 15:30:03 +0200

    Some userfriendliness for the levelsaver

--- Assets/_pROGRAMMING/LevelSaver.cs
+++ Assets/_pROGRAMMING/LevelSaver.cs
@@ -3,151 +3,222 @@ using System.Collections;
 using System.Xml;
 using System.Collections.Generic;
 
-
-
-public class LevelSaver : MonoBehaviour {
+public class LevelSaver : MonoBehaviour
+{
 
 	// Use this for initialization
 	
 	public string levelName = "";
-	
-	public bool shouldSave = false; //when set to true in the inspector it will save the level.
-	
-	
 	public string levelBase = "levelBase.xml";
 	public string outputFile = "levelOutput.xml";
-	
-	public List<Button3D> menuToSave;
-	public bool saveMenu = false;
+	private List<Button3D> _menuToSave;
 	public string menuName = "";
-	
-	void Start () {
+	public bool displaySaveMenu = false;
+	public Rect saveMenuRect = new Rect(0,0,200,300);
+	private string _feedback = "Save Menu";
+	private string _textInput = "";
+	private bool _drawHelp = true;
+	private enum MenuState{
+		Closed,
+		SelectingWhatToSave,
+		SavingLevel,
+		SavingMenu
+	}
+	private MenuState _state = MenuState.SelectingWhatToSave;
+	void Start ()
+	{
 		levelName = ""; // stops the editor from remembering
-		menuName = "";
-		saveMenu = false;
-		shouldSave = false;
-		menuToSave.Clear();
-		
+		menuName = "";		
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(shouldSave == true){
-			shouldSave = false;
-			if(levelName == ""){
-				Debug.LogError("Enter a level name first!");
-				return;
-			}
-			else{
-				SaveLevel();
-			}
-			
-		}
-		if(saveMenu == true){
-			saveMenu = false;
-			if(menuName == ""){
-				Debug.LogError("Enter a menu name first");
-				return;
-			}
-			else{
-				SaveMenu();
-			}
-		}
+	void Update ()
+	{
+
+		//SaveLevel();
+
+
+		//SaveMenu ();
+
 	
 	}
-	private void SaveLevel(){
-		Debug.Log("Saving level " + levelName);
+	void OnGUI(){
+		if(displaySaveMenu){
+			DrawSaveMenu();
+		}
+	}
+	
+	private void DrawSaveMenu(){
+		
+		GUILayout.BeginArea(saveMenuRect);
+		GUILayout.Label(_feedback);
+		if(_state == MenuState.SelectingWhatToSave){
+			if(GUILayout.Button("Write level file")){
+				_state = MenuState.SavingLevel;
+			}
+			else if(GUILayout.Button("Write menu file")){
+				_state = MenuState.SavingMenu;
+			}
+		}
+		else if(_state == MenuState.SavingLevel){
+			_feedback = "Saving a level, enter a name";
+			_textInput = GUILayout.TextField(_textInput, 15);
+			if(GUILayout.Button("Save level")){
+				levelName = _textInput;
+				SaveLevel();
+				_feedback = "Saved the level to " + Application.dataPath + "\\" +  outputFile;
+				_state = MenuState.SelectingWhatToSave;
+			}
+			BackButton();
+		}
+		else if(_state == MenuState.SavingMenu){
+			_feedback = "Saving a menu, enter a name";
+			_textInput = GUILayout.TextField(_textInput, 15);
+			if(GUILayout.Button("Save Menu")){
+				menuName = _textInput;
+				SaveMenu();
+				_feedback = "Saved the menu to " + Application.dataPath + "\\" + outputFile;
+				_state = MenuState.SelectingWhatToSave;
+			}
+			BackButton();
+			
+		}
+		else{
+		
+		}
+		
+		_drawHelp = GUILayout.Toggle(_drawHelp,"Help");
+		if(_drawHelp){
+			GUILayout.TextArea(HelpText());
+			
+		}
+		GUILayout.EndArea();
+	}
+	private string HelpText(){
+		if(_state == MenuState.SavingLevel){
+			return @"A level consist of castaways, triggers and ships.
+				Make sure that object with those scripts on them do not have parents.
+				To create a new castaway/ship drag and drop a castaway/ship from the prefab folder into the scene.
+				To create a trigger, create a new gameobject and click addcomponent, type in 'trigger'.
+				Go to the inspector of the gameobject, you can now set  the values of the  trigger.";
+		}
+		if(_state == MenuState.SavingMenu){
+			return @"To create a menu, drag and drop Button3D prefabs into the scene.
+			Position them where you like. Set the variables in the inspector of the gameobject.
+			command is  the function that will be called if the button is clicked. 
+			Ask a programmer what functions there are to call.";
+		}
+		return @"Select a target to save";
+	}
+	private void BackButton(){
+		if(GUILayout.Button("Back")){
+			_state = MenuState.SelectingWhatToSave;
+		}
+	}
+	private void SaveLevel ()
+	{
+		if (levelName == "") {
+			Debug.LogError ("Enter a level name first!");
+			return;
+		}
+		Debug.Log ("Saving level " + levelName);
 		
 		
-		XmlDocument doc = new XmlDocument();
-		doc.Load(Application.dataPath + "\\" +levelBase);
+		XmlDocument doc = new XmlDocument ();
+		doc.Load (Application.dataPath + "\\" + levelBase);
 		
 		XmlNode root = doc.DocumentElement;
 		
-		XmlNode testChild = doc.CreateNode(XmlNodeType.Element,"Level", "");
+		XmlNode testChild = doc.CreateNode (XmlNodeType.Element, "Level", "");
 		
 		XmlElement tc = (XmlElement)testChild;//
-		tc.SetAttribute("name", levelName);
+		tc.SetAttribute ("name", levelName);
 		
-		root.AppendChild(testChild);
+		root.AppendChild (testChild);
 		
-		XMLVisitor xmlVisitor = new XMLVisitor(doc);
+		XMLVisitor xmlVisitor = new XMLVisitor (doc);
 		
 //		IVisitable[] safeable;
-		GameObject[] allGos = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+		GameObject[] allGos = GameObject.FindObjectsOfType (typeof(GameObject)) as GameObject[];
 		
-		int safeLayer = LayerMask.NameToLayer("SaveLayer");
+		int safeLayer = LayerMask.NameToLayer ("SaveLayer");
 		
 		//finding all gos that are in the safelayer and have no parents
-		GameObject[] toSaveGo = System.Array.FindAll<GameObject>(allGos, x => x.layer == safeLayer && x.transform.parent == null);
+		GameObject[] toSaveGo = System.Array.FindAll<GameObject> (allGos, x => x.layer == safeLayer && x.transform.parent == null);
 	
-		Debug.Log("Gameobjects to save count = " + toSaveGo.Length);
+		Debug.Log ("Gameobjects to save count = " + toSaveGo.Length);
 				
 		int i = 0;
 		//System.Array.ForEach<GameObject>(toSaveGo, x => x.name = x.name + "_"+ i.ToString("000") + "_");
 		
-		foreach(GameObject go in toSaveGo){
+		foreach (GameObject go in toSaveGo) {
 			
-			go.name = go.name + "_" + i.ToString("000") + "_"; //give all gos that will be safed a unique names
+			go.name = go.name + "_" + i.ToString ("000") + "_"; //give all gos that will be safed a unique names
 			
 			i++;
 		}
-		foreach(GameObject go in toSaveGo){
-			xmlVisitor.OpenNewObject(go); // open a new gameobject to append the components to
+		foreach (GameObject go in toSaveGo) {
+			xmlVisitor.OpenNewObject (go); // open a new gameobject to append the components to
 			
-			List<IVisitable> toSave = new List<IVisitable>();
-			toSave.AddRange(go.GetInterfacesInChildren<IVisitable>()); // add all visitables to the list of what the visitor will pass
+			List<IVisitable> toSave = new List<IVisitable> ();
+			toSave.AddRange (go.GetInterfacesInChildren<IVisitable> ()); // add all visitables to the list of what the visitor will pass
 			
-			foreach(IVisitable visitable in toSave){
-				visitable.AcceptVisitor(xmlVisitor); // add each component to the active gameObject
+			foreach (IVisitable visitable in toSave) {
+				visitable.AcceptVisitor (xmlVisitor); // add each component to the active gameObject
 			}
 		
 		}
 		
 		//cleanup names
-		foreach(GameObject go in toSaveGo){
-			go.name = go.name.Substring(0, go.name.Length - 5); // leave the number out of it
+		foreach (GameObject go in toSaveGo) {
+			go.name = go.name.Substring (0, go.name.Length - 5); // leave the number out of it
 		}
 		
 		
 		
 //wrint to the output file. note that the asset database does not update automatically, minimize / reload unity first
-		XmlTextWriter writer = new XmlTextWriter(Application.dataPath + "\\" +outputFile, System.Text.Encoding.ASCII);
+		XmlTextWriter writer = new XmlTextWriter (Application.dataPath + "\\" + outputFile, System.Text.Encoding.ASCII);
 		writer.Formatting = Formatting.Indented;
 		//doc.Save(Application.dataPath + "\\" +outputFile);
-		doc.Save(writer);
-		writer.Close();
+		doc.Save (writer);
+		writer.Close ();
 		
 		
 		//doc.WriteContentTo(writer);
 		
 	}
 	
-	private void SaveMenu(){
-		Debug.Log("Saving menu " + menuName);
+	private void SaveMenu ()
+	{
+		if (menuName == "") {
+			Debug.LogError ("Enter a menu name first");
+			return;
+		}
+		Debug.Log ("Saving menu " + menuName);
 		
 		
-		XmlDocument doc = new XmlDocument();
-		doc.Load(Application.dataPath + "\\" +levelBase);
+		XmlDocument doc = new XmlDocument ();
+		doc.Load (Application.dataPath + "\\" + levelBase);
 		
 		XmlNode root = doc.DocumentElement;
 		
-		XmlNode testChild = doc.CreateNode(XmlNodeType.Element,"Menu", "");
+		XmlNode testChild = doc.CreateNode (XmlNodeType.Element, "Menu", "");
 		
 		
 		XmlElement tc = (XmlElement)testChild;//
-		tc.SetAttribute("name", menuName);
+		tc.SetAttribute ("name", menuName);
 		
-		root.AppendChild(testChild);
+		root.AppendChild (testChild);
 		
-		XMLVisitor xmlVisitor = new XMLVisitor(doc);
-		menuToSave.ForEach(x => x.AcceptVisitor(xmlVisitor));
+		XMLVisitor xmlVisitor = new XMLVisitor (doc);
+		_menuToSave = new List<Button3D>(FindObjectsOfType(typeof(Button3D)) as Button3D[]);
+		_menuToSave.ForEach (x => x.AcceptVisitor (xmlVisitor));
 		
-		XmlTextWriter writer = new XmlTextWriter(Application.dataPath + "\\" +outputFile, System.Text.Encoding.ASCII);
+		XmlTextWriter writer = new XmlTextWriter (Application.dataPath + "\\" + outputFile, System.Text.Encoding.ASCII);
 		writer.Formatting = Formatting.Indented;
 		//doc.Save(Application.dataPath + "\\" +outputFile);
-		doc.Save(writer);
-		writer.Close();
+		doc.Save (writer);
+		writer.Close ();
 	}
 }
