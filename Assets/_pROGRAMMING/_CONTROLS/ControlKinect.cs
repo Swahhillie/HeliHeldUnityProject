b547793f0e6776452d63afd4c60be7127d371391
David Hoogenbosch <DavidHoogenbosch@hotmail.com>
2013-05-14 15:03:51 +0200

    Heli Kinect controls update

--- Assets/_pROGRAMMING/_CONTROLS/ControlKinect.cs
+++ Assets/_pROGRAMMING/_CONTROLS/ControlKinect.cs
@@ -4,10 +4,9 @@ using System.Collections;
 public class ControlKinect : ControlBase
 {
 	public float forceThresshold = .5f;
-	public float steerSensitivity = 5.0f;
+	public float steerSensitivity = 4.0f;
 	
 	private KinectGestures kg;
-	private GUIStyle guistyle;
 	
 	Vector3 shouldC;
 	Vector3 handR;
@@ -18,42 +17,89 @@ public class ControlKinect : ControlBase
 	void Start()
 	{
 		kg = new KinectGestures(skelWrap);
-		guistyle = new GUIStyle();
-		guistyle.fontSize = 56;
-	}
-	string test;
-	void Update()
-	{
-		if(kg.GetHandsApart())
-		{
-			Vector3 dir = Vector3.zero;
-			handR = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.HandRight];
-			elbowR = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.ElbowRight];
-			handL = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.HandLeft];
-			elbowL = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.ElbowLeft];
-			
-			Vector3 diff = (handR + handL)*0.5f - (elbowR + elbowL)*0.5f;
-			
-			float steer = diff.x * steerSensitivity;
-			
-			dir.z = 0.5f - diff.y;
-			
-			dir = dir.magnitude > forceThresshold ? Vector3.Normalize(dir) : Vector3.zero;
-			
-			test = dir.z.ToString();
-			
-			heli.Steer(steer);
-			heli.Accelerate(dir);
-		}
-		else
-		{
-			cursorPosition = kg.GetCursorPosition();
-		}
 	}
 	
-	void OnGUI()
+	void Update()
 	{
-		GUI.contentColor = Color.green;
-		GUI.Label(new Rect(100, 100, 300, 300), test, guistyle);
+		float oldVal;
+		
+		if(!kg.GetHandsApart()) //hands tight
+		{
+			//set handstight time and save old value
+			oldVal = actionTimers[Action.HandsTight];
+			actionTimers[Action.HandsTight] += Time.deltaTime;
+			Debug.Log(actionTimers[Action.HandsTight]);
+			
+			//if handstight is hold longer then set value...
+			if(actionTimers[Action.HandsTight] > actionTime)
+			{
+				//reset handsapart
+				actionActive[Action.HandsApart] = false;
+				actionTimers[Action.HandsApart] = 0.0f;
+				
+				actionActive[Action.HandsTight] = true;
+				if(oldVal < actionTime) heli.EnterFlyMode(); //if old value was smaller, set transition
+				
+				//calculate flying variables
+				Vector3 dir = Vector3.zero;
+				handR = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.HandRight];
+				elbowR = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.ElbowRight];
+				handL = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.HandLeft];
+				elbowL = skelWrap.bonePos[0, (int)Kinect.NuiSkeletonPositionIndex.ElbowLeft];
+				
+				Vector3 diff = (handR + handL)*0.5f - (elbowR + elbowL)*0.5f;
+				
+				float steer = diff.x * steerSensitivity;
+				
+				dir.z = 0.5f - diff.y;
+				
+				dir = dir.magnitude > forceThresshold ? Vector3.Normalize(dir) : Vector3.zero;
+				
+				heli.Steer(steer);
+				heli.Accelerate(dir);
+			}
+		}
+		else //hands apart
+		{
+			//hands are apart
+			oldVal = actionTimers[Action.HandsApart];
+			actionTimers[Action.HandsApart] += Time.deltaTime;
+			
+			if(actionTimers[Action.HandsApart] > actionTime) //activated
+			{
+				//reset handstight
+				actionActive[Action.HandsTight] = false;
+				actionTimers[Action.HandsTight] = 0.0f;
+				
+				actionActive[Action.HandsApart] = true;
+				if(oldVal < actionTime) heli.EnterSaveMode(); //transition
+				
+				cursorPosition = kg.GetCursorPosition();
+				//give position to helicopter which handles it in saving mode
+			}
+			
+			if(kg.GetRadioGesture()) //radio gesture on
+			{
+				oldVal = actionTimers[Action.Radio];
+				actionTimers[Action.Radio] += Time.deltaTime;
+				
+				if(actionTimers[Action.Radio] > actionTime)
+				{
+					actionActive[Action.Radio] = true;
+					if(oldVal < actionTime)
+					{
+						heli.ActivateRadio();
+					}
+				}
+			}
+			else //no radio gesture
+			{
+				if(actionActive[Action.Radio] == true) //if active, disable radio
+					heli.DeactivateRadio();
+				
+				actionActive[Action.Radio] = false;
+				actionTimers[Action.Radio] = 0.0f;
+			}
+		}
 	}
 }
