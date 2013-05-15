c7d2d4eb78d1127e6a1582041001de21c8b30a39
David Hoogenbosch <davidhoogenbosch@hotmail.com>
2013-05-16 00:30:26 +0200

    small improvement to the LevelLoader

--- Assets/LoadAuto.cs
+++ Assets/LoadAuto.cs
@@ -4,10 +4,21 @@ using System.Collections;
 public class LoadAuto : MonoBehaviour {
 
 	ConfigLoader loader;
-	public string levelToLoad;
+	
+	public enum ToLoad
+	{
+		Level,
+		Menu
+	}
+	public ToLoad toLoad = ToLoad.Level;
+	public string toLoadName;
+	
 	void Start () {
 		loader = GetComponent<ConfigLoader>();
-		loader.LoadLevel(levelToLoad);
+		if(toLoad == ToLoad.Level)
+			loader.LoadLevel(toLoadName);
+		else if(toLoad == ToLoad.Menu)
+			loader.LoadMenu(toLoadName);
 	}
 
 }
