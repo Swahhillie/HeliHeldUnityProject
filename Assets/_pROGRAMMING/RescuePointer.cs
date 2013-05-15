7e3e53a16bb630bdd1dff7be35aa8222e0208348
David Hoogenbosch <davidhoogenbosch@hotmail.com>
2013-05-15 21:52:36 +0200

    Change to rescue pointer

--- Assets/_pROGRAMMING/RescuePointer.cs
+++ Assets/_pROGRAMMING/RescuePointer.cs
@@ -8,13 +8,15 @@ public class RescuePointer : MonoBehaviour {
 	public Transform endPos;
 	public Transform pointer;
 	
-	public float min = .6;
+	public float min = .6f;
 	public float max = 1.0f;
 	public float alpha;
-	
+	public float result;
 	// Update is called once per frame
 	void Update () {
-
+		
+		float a = (alpha - min) * (max /(max - min));
+		result = a;
 		pointer.transform.position = Vector3.Lerp(startPos.position, endPos.position, a);
 	}
 }
