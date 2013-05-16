3a92f0abb2699bf6d489aaec3ec1858d0725fe6c
BananenKiste <daniel-theunissen@freenet.de>
2013-05-16 02:54:24 +0200

    highlight shader

--- Assets/LoadAuto.cs
+++ Assets/LoadAuto.cs
@@ -5,6 +5,7 @@ public class LoadAuto : MonoBehaviour {
 
 	ConfigLoader loader;
 	public string levelToLoad;
+	
 	void Start () {
 		loader = GetComponent<ConfigLoader>();
 		loader.LoadLevel(levelToLoad);
