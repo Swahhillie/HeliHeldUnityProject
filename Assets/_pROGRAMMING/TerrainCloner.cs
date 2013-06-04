3522b3bd6df82c23feb15e4620676ab60fbe16e0
Dennis Roerdink <dennis_r@live.nl>
2013-06-04 11:26:20 +0200

    Latest version
    Radar. XML and stuff

--- Assets/_pROGRAMMING/TerrainCloner.cs
+++ Assets/_pROGRAMMING/TerrainCloner.cs
@@ -5,14 +5,20 @@ public class TerrainCloner : MonoBehaviour {
 
 	public Vector3 AdjustPosition;
 	public Material CloneMaterial;
+<<<<<<< HEAD
 	public int Layer;
+=======
+>>>>>>> Latest version
 	
 	void Awake()
 	{
 		Terrain clone = (Terrain)Instantiate(this.gameObject);
 		clone.transform.position = this.transform.position+AdjustPosition;
 		clone.renderer.material = CloneMaterial;
+<<<<<<< HEAD
 		clone.gameObject.layer = 1<<Layer;
+=======
+>>>>>>> Latest version
 	}
 	
 }
