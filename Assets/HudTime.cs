5a6fce1fc455c42bac6575cf8183b169fea2809d
Daniel Theunissen <daniel-theunissen@freenet.de>
2013-06-07 15:57:03 +0200

    prep kinectlistener and special mission

--- Assets/HudTime.cs
+++ Assets/HudTime.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class HudTime : MonoBehaviour 
 {
 	private float _startTime;
-	private GUIText guiText;
+	private GUIText _guiText;
 	
 	void Start () 
 	{
 		_startTime = Time.time;
-		guiText = this.gameObject.GetComponent<GUIText>();
+		_guiText = this.gameObject.GetComponent<GUIText>();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +17,6 @@ public class HudTime : MonoBehaviour
 	{
 		int minutes = (int)Time.time / 60;
 		float seconds = Time.time % 60;
-		guiText.text = minutes.ToString("00") + ":" +seconds.ToString("00.00");
+		_guiText.text = minutes.ToString("00") + ":" +seconds.ToString("00.00");
 	}
 }
